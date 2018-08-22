1294c97f82331d14e886dec9ed9cd4c0685be9a9
Kaniarasan Nagalingam <kaniarasan.nagalingam@Aspiresys.com>
2018-08-22 16:27:50 +0530

    Added Loader icon

--- MoneyLender/src/shared/auth.interceptor.ts
+++ MoneyLender/src/shared/auth.interceptor.ts
@@ -3,19 +3,43 @@ import {
   HttpEvent,
   HttpHandler,
   HttpInterceptor,
-  HttpRequest
+  HttpRequest,
+  HttpResponse
 } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
-import { Observable } from "rxjs/Observable";
+import { Observable } from "rxjs/Rx";
+import "rxjs/add/operator/map";
+import { Subject } from "rxjs/Subject";
+import { LoadingController } from "ionic-angular";
+import 'rxjs/add/operator/finally';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private common: common) {}
+  private noOfActiveRequest: number = 0;
+  private showHideLoader$ = new Subject<number>();
+  private showLoadIcon: boolean = false;
+  private dismissEvent$ = new Subject<boolean>();
+  public loading = this.loadingCtrl.create({
+    content: "Please wait...",
+    dismissOnPageChange:true
+  });
+  constructor(private common: common, public loadingCtrl: LoadingController) {
+    this.showHideLoader$.subscribe(x => {
+      if (!this.showLoadIcon && x > 0) {
+        this.showLoadIcon = true;
+        this.loading.present();
+      }
+    });
+  }
+
   intercept(
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
+    debugger;
+    this.noOfActiveRequest++;
+    this.showHideLoader$.next(this.noOfActiveRequest);
     if (
       this.common.AuthenticationToken != null &&
       this.common.AuthenticationToken.length > 0
@@ -28,6 +52,16 @@ export class AuthInterceptor implements HttpInterceptor {
         }
       });
     }
-    return next.handle(req);
+    return next.handle(req).finally(() => this.hideShowLoadingIcon(this));
+  }
+  hideShowLoadingIcon(event:any):void{
+    debugger;
+    if (event instanceof HttpResponse) {
+    if (this.showLoadIcon && this.noOfActiveRequest >= 0) {
+      this.showLoadIcon = false;
+      this.noOfActiveRequest = 0;
+      this.loading.dismiss();
+    }
+  }
   }
 }
